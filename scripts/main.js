246773cdd54c09e8c638a7ae337e0dab252a6b23
prashanth <nocturnaldevops@gmail.com>
2024-08-21 20:33:37 +0530

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+// This is the main JavaScript file for the rainbow-themed website
+// Add your JavaScript code here to add interactivity and functionality to the website
+
+// Example code: Change the background color of the page
+document.body.style.backgroundColor = 'rainbow';
